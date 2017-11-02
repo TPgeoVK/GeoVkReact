47a141ce7af2bbf4efb8a95571874e5fcb7a239c
Max Zaytsev <zaytsev@mail.com>
2017-11-02 14:18:45 +0300

    add actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,11 @@
+export const UPDATE_CHECKINS = 'LOAD_CHECKINS';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const CHECKIN = 'CHECKIN';
+export const CREATE_POST = 'CREATE_POST';
+export const WRITING_POST = 'WRITING_POST'; //user writing post... 
+                                            //...we already know where he is 
+                                            //...behind him have already gone
+                                            //...Oh! So wonderful that we live in the country 
+                                            //where you can be imprisoned for the post...
+                                            //it would seem: where is the Redux?
